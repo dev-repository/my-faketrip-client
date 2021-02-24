2adce0fcc318c72445dbb85f692db27dc5ca77ab
Hangeony <geonhui95@naver.com>
2021-02-25 00:53:27 +0900

    superKimoJJi

---
+++ assets/js/search.js
@@ -0,0 +1,80 @@
+var searchs = [
+    {
+       "product_id":null,
+       "id":33985,
+       "locale":"ko",
+       "status":"onsale",
+       "score":4.48275862068965,
+       "title":"샌디에이고 : 씨월드 입장권",
+       "subtitle":"놀라운 해양생물이 한가득! 세계 최대의 씨월드 중 한 곳, 샌디에이고 씨월드입니다.",
+       "ignore_wait_confirm":false,
+       "type":"ETicket",
+       "allow_quick_reservation":true,
+       "price_guarantee":false,
+       "city_name":"샌디에이고",
+       "languages":[
+          
+       ],
+       "meeting_time":null,
+       "duration":"",
+       "reviews_count":29,
+       "istanbul_reviews_count":0,
+       "istanbul_reviews_score":0,
+       "category_info":{
+          "code":"ticket",
+          "name":"티켓/패스"
+       },
+       "ext_v2_lv1_category":{
+          "code":"ticket",
+          "name":"입장권"
+       },
+       "main_price":{
+          "amount":73800.0,
+          "currency_code":"KRW",
+          "currency_symbol":"₩",
+          "present":"₩ 73,800",
+          "present_unit":"73,800원",
+          "krw_amount":73800
+       },
+       "original_price":{
+          "amount":null,
+          "currency_code":"KRW",
+          "currency_symbol":"₩",
+          "present":null,
+          "present_unit":null,
+          "krw_amount":null
+       },
+       "photos":[
+          {
+             "offer_id":33985,
+             "position":1,
+             "description":null,
+             "url_style_pattern":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_{style}_1525780549.jpg?1525780549",
+             "urls":{
+                "original":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_original_1525780549.jpg?1525780549",
+                "large":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_large_1525780549.jpg?1525780549",
+                "medium":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_medium_1525780549.jpg?1525780549",
+                "small":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_small_1525780549.jpg?1525780549",
+                "thumb":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_thumb_1525780549.jpg?1525780549",
+                "tiny":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_tiny_1525780549.jpg?1525780549",
+                "regular_medium":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_regular_medium_1525780549.jpg?1525780549"
+             },
+             "product_url":"https://d2ur7st6jjikze.cloudfront.net/offer_photos/33985/212522_original_1525780549.jpg?1525780549"
+          }
+       ],
+       "guide":{
+          "name":"KLK Technology",
+          "real_guide":false
+       },
+       "city_infos":[
+          {
+             "key_name":"San Diego",
+             "name":"샌디에이고",
+             "country_info":{
+                "key_name":"United States of America",
+                "name":"미국"
+             }
+          }
+       ],
+       "search_score":5.0
+    }]
